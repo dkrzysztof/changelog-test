1f41eb894e52d9c5c21848b6527caf2dc03b31ed
Krzysztof <krzysztof.dydak@it-solve.pl>
2024-05-09 18:14:16 +0200

    feat!: added husky and commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  helpUrl:
+    'https://bookstack.it-solve.pl/books/podstawka-frontend/page/informacje-ogolne-typescript',
+};
