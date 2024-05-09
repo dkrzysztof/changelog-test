e8427451c1a2a165ea8199337495183ee384bb0c
Krzysztof <krzysztof.dydak@it-solve.pl>
2024-05-09 15:26:23 +0200

    refactor(frontend): removed unused code

--- apps/frontend/src/App/Home/HomePage/HomePage.tsx
+++ apps/frontend/src/App/Home/HomePage/HomePage.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
-import { Form } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-
-import CreateEventForm, {
-  CreateEventFormFields,
-} from '../CreateEventForm/CreateEventForm';
-
-import { createEvent } from 'App/state/events/events.thunk';
-import { RootState } from 'App/state/root.reducer';
-import { isStatusLoading } from 'App/utils/checkStatusType';
-
-const createEventLoadingStatusSelector = (state: RootState) =>
-  isStatusLoading(state.events.status.createEvent);
 
 const HomePage: React.FC = () => {
-  const dispatch = useDispatch();
-  const [form] = Form.useForm();
-
-  const handleFormFinish = (values: CreateEventFormFields) =>
-    dispatch(createEvent(values));
-  const isLoading = useSelector(createEventLoadingStatusSelector);
-
   return (
     <>
       <div className="app-header d-flex-center-center">
